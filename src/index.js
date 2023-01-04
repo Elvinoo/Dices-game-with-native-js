669d342d6d0ea032a80b7f4711b06d5ebf0724e2
Elvinoo <elvinooo1990@gmail.com>
2023-01-05 02:44:11 +0400

    fonts called inside css and html validated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 let firstDice = document.querySelectorAll('.dice1 img');
 let secondDice = document.querySelectorAll('.dice2 img')
 let spinButton = document.querySelector('.spin');
-spinButton.addEventListener("click", spin)
+let h3 = document.createElement('h3');
+document.querySelector('.button').appendChild(h3);
+spinButton.addEventListener("click", spin);
 function spin() {
     firstDice.forEach(dice => dice.removeAttribute('class'));
     secondDice.forEach(dice => dice.removeAttribute('class'));
@@ -9,8 +11,8 @@ function spin() {
     let randomNumber2 = Math.floor(Math.random() * 6);
     firstDice[randomNumber1].setAttribute('class', "show");
     secondDice[randomNumber2].setAttribute('class', 'show');
-    let h3 = document.querySelector('h3');
-    randomNumber1 === randomNumber2 ? h3.innerText = 'DRAW!' : randomNumber1 < randomNumber2 ? h3.innerText = "Player 2 Wins! 🚩" : h3.innerText = "Player 1 Wins!🚩"
+    randomNumber1 === randomNumber2 ? h3.innerText = 'DRAW!' : randomNumber1 < randomNumber2 ? h3.innerText = "Player 2 Wins! 🚩" : h3.innerText = "Player 1 Wins!🚩";
+
 }
 document.querySelector('.restart').addEventListener('click', () => window.location.reload())
 
